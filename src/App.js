f19fc385fac9870c2eecd305d88965e7a3599476
mpr0xy <mpr0xy@live.com>
2021-09-27 22:16:14 +0800

    add data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useSpring, animated } from 'react-spring'
 import './App.css';
+import phoneticData from './data';
+
+const getRandomByLength = (length) => {
+  return Math.floor(Math.random() * 1000 % length);
+};
+
+const getRandomPhonetic = () => {
+  const pLength = phoneticData.length;
+  return phoneticData[getRandomByLength(pLength)];
+};
 
 function App() {
+  const [wordInfo, setWordInfo] = useState({});
+  const [isShowAns, setShowAns] = useState(false);
+  const [reverse, setReverse] = useState(false);
+  const animateStyle = useSpring({
+    to: async (next) => {
+      await next({ opacity: reverse ? 0 : 1 });
+    },
+    from: {
+      opacity: reverse ? 1 : 0
+    },
+    config: {
+      duration: 200
+    }
+  });
+  // 第一个题型，听到单词发音，写出单词音标。
+  // 会出现单词的拼写
+  const next = useCallback(() => {
+    // 现在随机一个音标
+    // 然后再音标里随机一个单词
+    setReverse(true);
+    const pLength = phoneticData.length;
+    const oneIndex = getRandomByLength(pLength);
+    const phonetic = phoneticData[oneIndex];
+    let wordList = [];
+    for (let letter in phonetic.letterCombination) {
+      wordList = wordList.concat(phonetic.letterCombination[letter]);
+    }
+    const wordIndex = getRandomByLength(wordList.length);
+    if (wordList[wordIndex].word === wordInfo.word) {
+      next();
+      return;
+    }
+    setTimeout(() => {
+      setShowAns(false);
+      setWordInfo(wordList[wordIndex]);
+      setReverse(false);
+    }, 200);
+  }, []);
+  const playAudio = useCallback(() => {
+    const audio = new Audio(`https://dict.youdao.com/dictvoice?audio=${wordInfo.word}&type=1`);
+    audio.play();
+  }, [wordInfo]);
+  useEffect(() => {
+    next();
+  }, [next]);
+  useEffect(() => {
+    playAudio();
+  }, [wordInfo, playAudio]);
+  const getRandomByLength = (length) => {
+    return Math.floor(Math.random() * 1000 % length);
+  };
+  console.log(getRandomPhonetic().pronunciation);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="layout">
+        <animated.div style={animateStyle} className="content">
+          <p className="word" onClick={playAudio}>{wordInfo.word}</p>
+          {
+            !isShowAns &&
+              <span className="ans" onClick={() => setShowAns(true)}>显示答案</span>
+          }
+          {
+            isShowAns &&
+              <span className="ans">{wordInfo.phonetic}</span>
+          }
+          <button className="nextText" onClick={next}>下一题</button>
+        </animated.div>
+        <audio controls src={getRandomPhonetic().pronunciation} />
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,342 @@
+const DataList = [
+  {
+    name: 'ɪ',
+    pronunciation: require('./assets/pronunciation/ɪ.mp3').default,
+    letterCombination: {
+      i: [
+        {
+          word: 'bit',
+          phonetic: '[bɪt]',
+        },
+        {
+          word: 'dinner',
+          phonetic: '[ˈdɪnə(r)]',
+        },
+        {
+          word: 'kit',
+          phonetic: '[kɪt]',
+        },
+        {
+          word: 'bid',
+          phonetic: '[bɪd]',
+        },
+        {
+          word: 'litter',
+          phonetic: '[ˈlɪtə(r)]'
+        },
+        {
+          word: 'chip',
+          phonetic: '[tʃɪp]'
+        },
+        {
+          word: 'fit',
+          phonetic: '[fɪt]'
+        },
+        {
+          word: 'his',
+          phonetic: '[hɪz]'
+        },
+        {
+          word: 'pitch',
+          phonetic: '[pɪtʃ]'
+        },
+        {
+          word: 'ship',
+          phonetic: '[ʃɪp]'
+        }
+      ],
+      a: [
+        {
+          word: 'orangeade',
+          phonetic: '[ˌɒrɪndʒˈeɪd]'
+        },
+        {
+          word: 'sausage',
+          phonetic: '[ˈsɒsɪdʒ]'
+        }
+      ],
+      ui: [
+        {
+          word: 'build',
+          phonetic: '[bɪld]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'iː',
+    pronunciation: require('./assets/pronunciation/iː.mp3').default,
+    letterCombination: {
+      i: [
+        {
+          word: 'litre',
+          phonetic: '[ˈliːtə(r)]'
+        }
+      ],
+      ee: [
+        {
+          word: 'tree',
+          phonetic: '[triː]'
+        },
+        {
+          word: 'feet',
+          phonetic: '[fiːt]'
+        },
+        {
+          word: 'sheep',
+          phonetic: '[ʃiːp]'
+        }
+      ],
+      ea: [
+        {
+          word: 'leave',
+          phonetic: '[liːv]'
+        },
+        {
+          word: 'cheap',
+          phonetic: '[tʃiːp]'
+        },
+        {
+          word: 'peach',
+          phonetic: '[piːtʃ]'
+        }
+      ],
+      ie: [
+        {
+          word: 'piece',
+          phonetic: '[piːs]'
+        }
+      ],
+      ei: [
+        {
+          word: 'receive',
+          phonetic: '[rɪˈsiːv]'
+        }
+      ],
+      e: [
+        {
+          word: 'he\'s',
+          phonetic: '[hiːz]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'e',
+    pronunciation: require('./assets/pronunciation/e.mp3').default,
+    letterCombination: {
+      a: [
+        {
+          word: 'many',
+          phonetic: '[ˈmeni]'
+        }
+      ],
+      e: [
+        {
+          word: 'desk',
+          phonetic: '[desk]'
+        }
+      ],
+      ai: [
+        {
+          word: 'said',
+          phonetic: '[sed]'
+        }
+      ],
+      ea: [
+        {
+          word: 'dead',
+          phonetic: '[ded]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'æ',
+    pronunciation: require('./assets/pronunciation/æ.mp3').default,
+    letterCombination: {
+      a: [
+        {
+          word: 'stand',
+          phonetic: '[stænd]'
+        },
+        {
+          word: 'family',
+          phonetic: '[ˈfæməli]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'ɒ',
+    pronunciation: require('./assets/pronunciation/ɒ.mp3').default,
+    letterCombination: {
+      o: [
+        {
+          word: 'collar',
+          phonetic: '[ˈkɒlə(r)]'
+        },
+        {
+          word: 'hot',
+          phonetic: '[hɒt]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'ɔː',
+    pronunciation: require('./assets/pronunciation/ɔː.mp3').default,
+    letterCombination: {
+      a: [
+        {
+          word: 'water',
+          phonetic: '[ˈwɔːtə(r)]'
+        },
+        {
+          word: 'Nepal',
+          phonetic: '[nɪˈpɔːl]'
+        }
+      ],
+      al: [
+        {
+          word: 'mall',
+          phonetic: '[mɔːl]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'ʊ',
+    pronunciation: require('./assets/pronunciation/ʊ.mp3').default,
+    letterCombination: {
+      o: [
+        {
+          word: 'woman',
+          phonetic: '[ˈwʊmən]'
+        },
+        {
+          word: 'wolf',
+          phonetic: '[wʊlf]'
+        }
+      ],
+      u: [
+        {
+          word: 'put',
+          phonetic: '[pʊt]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'uː',
+    pronunciation: require('./assets/pronunciation/uː.mp3').default,
+    letterCombination: {
+      o: [
+        {
+          word: 'improve',
+          phonetic: '[ɪmˈpruːv]'
+        },
+        {
+          word: 'movie',
+          phonetic: '[ˈmuːvi]'
+        }
+      ],
+      u: [
+        {
+          word: 'ruler',
+          phonetic: '[ˈruːlə(r)]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'ʌ',
+    pronunciation: require('./assets/pronunciation/ʌ.mp3').default,
+    letterCombination: {
+      o: [
+        {
+          word: 'come',
+          phonetic: '[kʌm]'
+        },
+        {
+          word: 'money',
+          phonetic: '[ˈmʌni]'
+        }
+      ],
+      u: [
+        {
+          word: 'shut',
+          phonetic: '[ʃʌt]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'ɑː',
+    pronunciation: require('./assets/pronunciation/ɑː.mp3').default,
+    letterCombination: {
+      a: [
+        {
+          word: 'after',
+          phonetic: '[ˈɑːftə(r)]'
+        },
+        {
+          word: 'ask',
+          phonetic: '[ɑːsk]'
+        }
+      ],
+      al: [
+        {
+          word: 'half',
+          phonetic: '[hɑːf]'
+        }
+      ]
+    }
+  },
+  {
+    name: 'ə',
+    pronunciation: require('./assets/pronunciation/ə.mp3').default,
+    letterCombination: {
+      a: [
+        {
+          word: 'banana',
+          phonetic: '[bəˈnɑːnə]'
+        },
+        {
+          word: 'address',
+          phonetic: '[əˈdres]'
+        }
+      ],
+      e: [
+        {
+          word: 'happen',
+          phonetic: '[ˈhæpən]'
+        }
+      ]
+    },
+  },
+  {
+    name: 'ɜː',
+    pronunciation: require('./assets/pronunciation/ɜː.mp3').default,
+    letterCombination: {
+      er: [
+        {
+          word: 'serve',
+          phonetic: '[sɜːv]'
+        },
+        {
+          word: 'nerd',
+          phonetic: '[nɜːd]'
+        }
+      ],
+      ir: [
+        {
+          word: 'girl',
+          phonetic: '[ɡɜːl]'
+        }
+      ]
+    }
+  }
+];
+
+export default DataList;
